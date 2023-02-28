e8e9bf8ae38df1bcce0ec5ebcd60a835b2ecf04c
Mariia Verdina <masha.verdina@gmail.com>
2023-02-28 20:10:24 +0300

    Implement 3-omit + fix code style

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -6,6 +6,6 @@
  */
 export const pick = (obj, ...fields) => {
   return Object.fromEntries(
-    Object.entries(obj).filter(([key, val]) => fields.includes(key))
+    Object.entries(obj).filter(([key]) => fields.includes(key))
   );
 };
