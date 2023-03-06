c7a4793baae67560561411f42c1427f6b0e3d98e
Mariia Verdina <masha.verdina@gmail.com>
2023-03-06 19:06:06 +0300

    Implement 3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {return string;}
+  let res = '';
+  for (let i = 0; i < string.length; i++) {
+    const repeatCount = countStartRepeats(string.slice(i));
+    if (repeatCount <= size) {
+      res += string.slice(i, i + repeatCount);
+    } else {
+      res += string.slice(i, i + size);
+    }
+    i += repeatCount - 1;
+  }
+  return res;
+}
 
+function countStartRepeats(string) {
+  if (string === undefined || string === '') {return 0;}
+  const c = string.at(0);
+  let count = 0;
+  while (c === string.at(count)) {count++;}
+  return count;
 }
